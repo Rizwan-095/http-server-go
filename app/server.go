32c0e24673b75572285973f8548dbd6eea92f2ad
rizwan_hodlneers <awanrizwan615@gmail.com>
2024-07-12 12:04:00 +0500

    pass 5th stage

--- app/server.go
+++ app/server.go
@@ -32,20 +32,23 @@ func main() {
 		fmt.Println("Error reading request: ", err.Error())
 		os.Exit(1)
 	}
-
+	request := string(req)
 	path := strings.Split(string(req), " ")[1]
-	headers := strings.Split(string(req), "\r\n")
 	if path == "/" {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	} else if strings.Split(path, "/")[1] == "echo" {
 		message := strings.Split(path, "/")[2]
 		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)))
 	} else if strings.Split(path, "/")[1] == "user-agent" {
-		headerValue := strings.TrimSpace(strings.TrimPrefix(headers[1], "User-Agent:"))
-		fmt.Println(headerValue)
-		if strings.HasPrefix(string(headers[1]), "User-Agent:") {
-			conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(headerValue), headerValue)))
+		headers := strings.Split(request, "\r\n")[1:]
+		userAgent := ""
+		for _, header := range headers {
+			if strings.HasPrefix(header, "User-Agent") {
+				userAgent = strings.Split(header, ": ")[1]
+			}
 		}
+		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)))
+
 	} else {
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
